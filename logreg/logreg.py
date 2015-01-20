b7475ddb8adefb1904548fe909f81389f395c87d
Jordan Boyd-Graber <jordanbg@gmail.com>
2015-01-20 15:04:50 -0700

    adding data

--- logreg/logreg.py
+++ logreg/logreg.py
@@ -145,11 +145,11 @@ if __name__ == "__main__":
     argparser.add_argument("--step", help="Initial SG step size",
                            type=float, default=0.1, required=False)
     argparser.add_argument("--positive", help="Positive class",
-                           type=str, default="positive", required=False)
+                           type=str, default="../data/hockey_baseball/positive", required=False)
     argparser.add_argument("--negative", help="Negative class",
-                           type=str, default="negative", required=False)
+                           type=str, default="../data/hockey_baseball/negative", required=False)
     argparser.add_argument("--vocab", help="Vocabulary that can be features",
-                           type=str, default="vocab", required=False)
+                           type=str, default="../data/hockey_baseball/vocab", required=False)
     argparser.add_argument("--passes", help="Number of passes through train",
                            type=int, default=1, required=False)
 
